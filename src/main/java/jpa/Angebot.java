d03132c23afcc09002e74f39ef81214b72d29610
vm <vm@vm-VirtualBox>
2018-02-28 11:00:21 +0100

    fix

--- src/main/java/jpa/Angebot.java
+++ src/main/java/jpa/Angebot.java
@@ -28,7 +28,7 @@ import javax.validation.constraints.Size;
  * Eine zu erledigende Aufgabe.
  */
 @Entity
-public class Task implements Serializable {
+public class Angebot implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -36,7 +36,33 @@ public class Task implements Serializable {
     @GeneratedValue(generator = "task_ids")
     @TableGenerator(name = "task_ids", initialValue = 0, allocationSize = 50)
     private long id;
+    
+    @NotNull(message = "Die Art darf nicht leer sein.")
+    private String art = "";
+    @NotNull(message = "Der Titel darf nicht leer sein.")
+    private String titel = "";
 
+    @Lob
+    @NotNull(message = "Die Beschreibung darf nicht leer sein.")
+    @Size(min = 1, max = 50, message = "Die Bezeichnung muss zwischen ein und 50 Zeichen lang sein.")
+    private String beschreibung = "";
+
+    private java.util.Date erstellungsDatum = null;
+    private java.util.Date onlineBis = null;
+
+    @NotNull(message = "Der Preis darf nicht leer sein.")
+    private double preisVorstellung = 0;
+    
+    @NotNull(message = "Die Preisart darf nicht leer sein.")
+    private String artDesPreises = "";
+
+    @NotNull(message = "Die PLZ darf nicht leer sein.")
+    private int plz = 0;
+    
+    @NotNull(message = "Der Ort darf nicht leer sein.")
+    private String ort = "";
+    
+    
     @ManyToOne
     @NotNull(message = "Die Aufgabe muss einem Benutzer geordnet werden.")
     private User owner;
@@ -44,46 +70,109 @@ public class Task implements Serializable {
     @ManyToOne
     private Category category;
 
-    @Column(length = 50)
-    @NotNull(message = "Die Bezeichnung darf nicht leer sein.")
-    @Size(min = 1, max = 50, message = "Die Bezeichnung muss zwischen ein und 50 Zeichen lang sein.")
-    private String shortText;
-
-    @Lob
-    @NotNull
-    private String longText;
-
-    @NotNull(message = "Das Datum darf nicht leer sein.")
-    private Date dueDate;
-
-    @NotNull(message = "Die Uhrzeit darf nicht leer sein.")
-    private Time dueTime;
-
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private TaskStatus status = TaskStatus.OPEN;
+    
 
     //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
-    public Task() {
+    public Angebot() {
     }
 
-    public Task(User owner, Category category, String shortText, String longText, Date dueDate, Time dueTime) {
+    public Angebot(User owner, Category category, String art, String titel, 
+            String beschreibung, Date erstellungsDatum, Time onlineBis,
+            Double preisVorstellung, String artDesPreises, int plz, String ort) {
         this.owner = owner;
         this.category = category;
-        this.shortText = shortText;
-        this.longText = longText;
-        this.dueDate = dueDate;
-        this.dueTime = dueTime;
+        this.art = art;
+        this.titel = titel;
+        this.beschreibung = beschreibung;
+        this.erstellungsDatum = erstellungsDatum;
+        this.onlineBis = onlineBis;
+        this.preisVorstellung = preisVorstellung;
+        this.artDesPreises = artDesPreises;
+        this.plz = plz;
+        this.ort = ort;
+        
+        
     }
     //</editor-fold>
 
-    //<editor-fold defaultstate="collapsed" desc="Setter und Getter">
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getArt() {
+        return art;
+    }
+
+    public void setArt(String art) {
+        this.art = art;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public java.util.Date getErstellungsDatum() {
+        return erstellungsDatum;
+    }
+
+    public void setErstellungsDatum(java.util.Date erstellungsDatum) {
+        this.erstellungsDatum = erstellungsDatum;
+    }
+
+    public java.util.Date getOnlineBis() {
+        return onlineBis;
+    }
+
+    public void setOnlineBis(java.util.Date onlineBis) {
+        this.onlineBis = onlineBis;
+    }
+
+    public double getPreisVorstellung() {
+        return preisVorstellung;
+    }
+
+    public void setPreisVorstellung(double preisVorstellung) {
+        this.preisVorstellung = preisVorstellung;
+    }
+
+    public String getArtDesPreises() {
+        return artDesPreises;
+    }
+
+    public void setArtDesPreises(String artDesPreises) {
+        this.artDesPreises = artDesPreises;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
     }
 
     public User getOwner() {
@@ -102,45 +191,5 @@ public class Task implements Serializable {
         this.category = category;
     }
 
-    public String getShortText() {
-        return shortText;
-    }
-
-    public void setShortText(String shortText) {
-        this.shortText = shortText;
-    }
-
-    public String getLongText() {
-        return longText;
-    }
-
-    public void setLongText(String longText) {
-        this.longText = longText;
-    }
-
-    public Date getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Time getDueTime() {
-        return dueTime;
-    }
-
-    public void setDueTime(Time dueTime) {
-        this.dueTime = dueTime;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-    //</editor-fold>
-
+    
 }
